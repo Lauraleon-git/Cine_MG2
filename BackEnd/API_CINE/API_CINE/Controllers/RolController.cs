f5d4cfd855917d481dbfec0ea36c9be7e3c2a604
Laura Leon <lauralglc93@gmail.com>
2024-01-15 23:42:50 -0400

    LauraLeon_Rol

---
+++ BackEnd/API_CINE/API_CINE/Controllers/RolController.cs
@@ -0,0 +1,89 @@
+﻿using API_CINE.Contexto;
+using API_CINE.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_CINE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+        private readonly CineContext _context;
+        public RolController(CineContext context)
+        {
+            _context= context;
+        }
+        // GET: api/<RolController>
+        [HttpGet]
+        public async Task<ActionResult<List<Rol>>> Get()
+        {
+            return await _context.Rols.ToListAsync();
+        }
+
+        // GET api/<RolController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rol>> Get(int id)
+        {
+            return await _context.Rols.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        // POST api/<RolController>
+        [HttpPost]
+        public async Task<ActionResult>Post(Rol rol)
+        {
+            if (rol != null)
+            {
+                _context.Rols.Add(rol);
+                await _context.SaveChangesAsync();
+                return Ok();
+
+            }
+            else
+            {
+                return BadRequest("Debe ingresar datos Validos ");
+            }
+        }
+        
+
+        
+
+        // PUT api/<RolController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Rol rol)
+        {
+            Rol rolModificar=await _context.Rols.FirstOrDefaultAsync(rol => rol.Id == id);
+            if(rolModificar != null)
+            {
+                rolModificar.Nombre= rol.Nombre;
+                rolModificar.Descripcion= rol.Descripcion;
+                rolModificar.Estado=rol.Estado;
+                await _context.SaveChangesAsync();
+                return Ok();
+
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE api/<RolController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id , Rol rol)
+
+        {
+            Rol rolEliminar = await _context.Rols.FirstOrDefaultAsync(r => r.Id == id);
+            if (rolEliminar != null)
+            {
+                _context.Remove(rolEliminar);
+                await _context.SaveChangesAsync();
+                return Ok();
+
+            }
+            else { return NotFound(); }
+        }
+    }
+}
--- BackEnd/API_CINE/API_CINE/obj/Debug/net8.0/API_CINE.AssemblyInfo.cs
+++ BackEnd/API_CINE/API_CINE/obj/Debug/net8.0/API_CINE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_CINE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca97f9ef5ba770c1b72a14272342af69cabf059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf1379868e23769863adbfe11b3beb1ed70766a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_CINE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_CINE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
