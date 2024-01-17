f5edf2a3ac5c972c8a5a28fcd332fbe2aa8d4828
JulioRuiz884 <pr724206@gmail.com>
2024-01-17 01:01:29 -0400

    JulioRuizEnvio2

---
+++ BackEnd/API_CINE/API_CINE/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using API_CINE.Contexto;
+using API_CINE.Modelos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_CINE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly CineContext _context;
+        public UsuarioController(CineContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public async Task<ActionResult<List<Usuario>>> Get()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> Get(int id)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
