390d44854d4af66fb43441c31b927073d969b508
JulioRuiz884 <pr724206@gmail.com>
2024-01-17 01:56:15 -0400

    JulioRuizEnvio4

--- BackEnd/API_CINE/API_CINE/Controllers/UsuarioController.cs
+++ BackEnd/API_CINE/API_CINE/Controllers/UsuarioController.cs
@@ -29,7 +29,7 @@ namespace API_CINE.Controllers
             return await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
         }
 
-        // POST api/<PersonasController>
+        // POST api/<UsuarioController>
         [HttpPost]
         public async Task<ActionResult> Post(Usuario usuario)
         {
@@ -42,7 +42,7 @@ namespace API_CINE.Controllers
             else { return BadRequest("Debe ingresar datos validos"); }
         }
 
-        // PUT api/<PersonasController>/5
+        // PUT api/<UsuarioController>/5
         [HttpPut("{id}")]
         public async Task<ActionResult> Put(int id, Usuario usuario)
         {
@@ -58,6 +58,74 @@ namespace API_CINE.Controllers
                 return Ok();
             }
             else { return NotFound(); }
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Usuario usuarioEliminar = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+            if (usuarioEliminar != null)
+            {
+                _context.Remove(usuarioEliminar);
+
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            else { return NotFound(); }
+        }
+
+
+        [HttpGet("reporte1")]
+        public async Task<ActionResult<List<reporte1>>> ListarUsuariosRol()
+        {
+            var consulta = await _context.Usuarios.Select(x => new reporte1
+            {
+                Idusuario = x.Id,
+                Usuario = x.Usuario1,
+                Contrasenia = x.Contraseña,
+                NombreRol = x.Roles.Nombre,
+                EstadoRol = x.Roles.Estado,
+            }).ToListAsync();
+            return Ok(consulta);
+        }
+        public class reporte1
+        {
+            public int? Idusuario { get; set; }
+            public string? Usuario { get; set; }
+            public string? Contrasenia { get; set; }
+            public string? NombreRol { get; set; }
+            public string? EstadoRol { get; set; }
+        }
+        [HttpGet("reporte2")]
+        public async Task<ActionResult<List<reporte2>>> ListarPersonasUsuariosRol()
+        {
+            var consulta = await _context.Usuarios.Select(x => new reporte2
+            {
+                Idusuario = x.Id,
+                Usuario = x.Usuario1,
+                Contrasenia = x.Contraseña,
+                NombreRol = x.Roles.Nombre,
+                NombrePersona = x.Personas.Nombre,
+                ApellidoPersona = x.Personas.Apellido,
+                TelefonoPersona = x.Personas.Telefono,
+                CiPersona = x.Personas.Ci,
+                EstadoRol = x.Roles.Estado,
+
+            }).ToListAsync();
+            return Ok(consulta);
+        }
+        public class reporte2
+        {
+            public int? Idusuario { get; set; }
+            public string? Usuario { get; set; }
+            public string? Contrasenia { get; set; }
+            public string? NombreRol { get; set; }
+            public string? EstadoRol { get; set; }
+            public string? NombrePersona { get; set; }
+            public string? ApellidoPersona { get; set; }
+            public int? TelefonoPersona { get; set; }
+            public int? CiPersona { get; set; }
         }
     }
 }
