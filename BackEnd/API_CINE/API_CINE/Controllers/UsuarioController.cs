526a7f209786053b0953300894bbdc1d37e760af
JulioRuiz884 <pr724206@gmail.com>
2024-01-17 01:14:00 -0400

    JulioRuizEnvio3

--- BackEnd/API_CINE/API_CINE/Controllers/UsuarioController.cs
+++ BackEnd/API_CINE/API_CINE/Controllers/UsuarioController.cs
@@ -28,5 +28,36 @@ namespace API_CINE.Controllers
         {
             return await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
         }
+
+        // POST api/<PersonasController>
+        [HttpPost]
+        public async Task<ActionResult> Post(Usuario usuario)
+        {
+            if (usuario != null)
+            {
+                _context.Usuarios.Add(usuario);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            else { return BadRequest("Debe ingresar datos validos"); }
+        }
+
+        // PUT api/<PersonasController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Usuario usuario)
+        {
+            Usuario usuarioModificar = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+            if (usuarioModificar != null)
+            {
+                usuarioModificar.Usuario1 = usuario.Usuario1;
+                usuarioModificar.Contraseña = usuario.Contraseña;
+                usuarioModificar.Fecha = usuario.Fecha;
+                usuarioModificar.Estado = usuario.Estado;
+
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            else { return NotFound(); }
+        }
     }
 }
